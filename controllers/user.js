cc3614197e9d6efe050d6f173c547a0600d3079b
Kirill-jing <kirill.primachenko@gmail.com>
2020-08-25 23:34:33 +0300

    f

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,55 @@
 const User = require('../models/User')
 const {validationResult} = require('express-validator')
+const bcrypt = require('bcryptjs')
+const jwt=require('jsonwebtoken')
 
 exports.signup=(req,res,next)=>{
     let name=req.body.name
     let email=req.body.email
     let password = req.body.password
-    const errors= validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(422).json({
-            message:'validation failed',
-            errors:errors.array()
+    // const errors= validationResult(req)
+    // if(!errors.isEmpty()){
+    //     return res.status(422).json({
+    //         message:'validation failed',
+    //         errors:errors.array()
+    //     })
+    // }
+    bcrypt.hash(password,12).then(hashedPw=>{
+        const user =new User({
+            email:email,
+            name:name,
+            password:hashedPw
         })
-    }
+        return user.save()
+    }).then(result=>{
+        res.status(201).json({message:'user created',userId:result._id})
+    })
+    .catch(err=>console.log(err))
 
+}
+
+exports.login=(req,res,next)=>{
+    const email=req.body.email
+    const password = req.body.password
+    let loadedUser;
+    User.findOne({email:email}).then(user=>{
+        if(!user){
+            res.status(401).json({
+                message:'no user found'
+            })
+        }
+        loadedUser=user
+      return  bcrypt.compare(password,user.password)
+    }).then(isEqual=>{
+        if(!isEqual){
+            res.status(401).json({
+                message:'wrong password'
+            })
+        }
+        const token= jwt.sign({email:loadedUser.email,id:loadedUser._id.toString()},'secret',{expiresIn:'1h'})
+        res.status(200).json({
+            token:token,id:loadedUser._id.toString()
+        })
+    })
+.catch(err=>console.log(err))
 }
--- routes/aurh.js
+++ routes/aurh.js
@@ -4,16 +4,18 @@ const router =express.Router()
 const authController=require('../controllers/user')
 const User=require('../models/User')
 router.put('/signup',[
-    body('email').isEmail()
+    body('email')
     .custom((value,{req})=>{
         User.findOne({email:value}).then(user=>{
             if(user){
                 return Promise.reject('email already exists')
             }
         })
-    }).normalizeEmail(),
+    }),
     body('password').trim().isLength({min:5}),
     body('name').trim().not().isEmpty()
 ],authController.signup)
+
+router.post('/login',authController.login)
 
 module.exports=router
