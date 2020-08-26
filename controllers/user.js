fa7def9e64495d1cf09d02e50b53daed35b9e12b
Kirill-jing <kirill.primachenko@gmail.com>
2020-08-26 23:29:34 +0300

    y

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const User = require('../models/User')
 const {validationResult} = require('express-validator')
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcrypt')
 const jwt=require('jsonwebtoken')
 
 exports.signup=(req,res,next)=>{
@@ -31,25 +31,35 @@ exports.signup=(req,res,next)=>{
 exports.login=(req,res,next)=>{
     const email=req.body.email
     const password = req.body.password
+  
     let loadedUser;
     User.findOne({email:email}).then(user=>{
         if(!user){
-            res.status(401).json({
+          return  res.status(401).json({
                 message:'no user found'
             })
+
         }
         loadedUser=user
-      return  bcrypt.compare(password,user.password)
+        console.log(user.password)
+        console.log(password)
+        
+      return  bcrypt.compare(password,user.password
+      )
+
     }).then(isEqual=>{
-        if(!isEqual){
-            res.status(401).json({
+        console.log(isEqual)
+        if(isEqual==false){
+         return (  res.status(401).json({
                 message:'wrong password'
-            })
+            }))
         }
         const token= jwt.sign({email:loadedUser.email,id:loadedUser._id.toString()},'secret',{expiresIn:'1h'})
-        res.status(200).json({
+       res.status(200).json({
             token:token,id:loadedUser._id.toString()
         })
-    })
-.catch(err=>console.log(err))
+    }).catch(err=>console.log(err)
+    
+    )
 }
+
